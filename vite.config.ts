aecec781f385d22e5b77d1bccfb2581c19860630
miguelharukikurahassi <miguelharuki.kurahassi@gmail.com>
2026-04-27 14:59:11 -0300

    Resolvendo problemas

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // CRUCIAL for GitHub Pages to resolve assets correctly
+  base: '/Osint/', // CRUCIAL: Nome do repositório no GitHub para os assets carregarem
 })
